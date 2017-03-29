3f41493b8f5853ceef615eb245ad31d1e77006f5
SangLiang <378305868@qq.com>
2017-03-30 00:02:06 +0800

    some changed

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 var StartScene = require("./modules/scenes/StartScene");
 var GameScene = require("./modules/scenes/GameScene");
-var Preload = require("./modules/Preload");
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO,'mainCanvas', {},true);
 
 game.state.add("StartScene",StartScene);  // 游戏开始场景
 game.state.add("GameScene",GameScene);    // 游戏场景
-game.state.add("Preload", Preload);       // 游戏加载场景
-game.state.start("Preload");
+game.state.start("StartScene");
