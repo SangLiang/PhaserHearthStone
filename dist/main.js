61ef7d88289d72797abe85d55e7be4cf22ee6f3b
sangliang <378305868@qq.com>
2017-06-06 16:52:37 +0800

    v1.2.0

--- dist/main.js
+++ dist/main.js
@@ -1357,31 +1357,31 @@ function CardChoiseScene(game) {
         // 锚点设置
         loadText.anchor.set(0.5);
 
-        game.load.image("background", "../../resource/background.png");
-        game.load.image("card_back", "../../resource/card_back.png");
-        game.load.image("enemy_turn_button", "../../resource/enemy_turn_button.png");
-        game.load.image("fee", "../../resource/fee.png");
-        game.load.image("hero_turn_button", "../../resource/hero_turn_button.png");
-        game.load.image("hp_background", "../../resource/hp_background.png");
-        game.load.image("attack_icon","../../resource/attack_icon.png");
-        game.load.image("shot_card","../../resource/shot_card.png");
-        game.load.image("fighter_hero", "../../resource/fighter_hero.png");
-        game.load.image("choiseScene_bg","../../resource/choiseScene_bg.jpg");
-        game.load.image("confirm_btn","../../resource/confirm_btn.png");
+        game.load.image("background", "./resource/background.png");
+        game.load.image("card_back", "./resource/card_back.png");
+        game.load.image("enemy_turn_button", "./resource/enemy_turn_button.png");
+        game.load.image("fee", "./resource/fee.png");
+        game.load.image("hero_turn_button", "./resource/hero_turn_button.png");
+        game.load.image("hp_background", "./resource/hp_background.png");
+        game.load.image("attack_icon","./resource/attack_icon.png");
+        game.load.image("shot_card","./resource/shot_card.png");
+        game.load.image("fighter_hero", "./resource/fighter_hero.png");
+        game.load.image("choiseScene_bg","./resource/choiseScene_bg.jpg");
+        game.load.image("confirm_btn","./resource/confirm_btn.png");
 
-        game.load.image("arcaneWisdom","../../resource/arcaneWisdom.png");
-        game.load.image("dead_wing", "../../resource/dead_wing.png");
-        game.load.image("dead_wing_fight", "../../resource/dead_wing_fight.png");
-        game.load.image("fishman_baby", "../../resource/fishman_baby.png");
-        game.load.image("fishman_baby_fight", "../../resource/fishman_baby_fight.png");
-        game.load.image("freshwater_crocodile", "../../resource/freshwater_crocodile.png");
-        game.load.image("freshwater_crocodile_fight", "../../resource/freshwater_crocodile_fight.png");
-        game.load.image("ogre", "../../resource/ogre.png");
-        game.load.image("ogre_fight", "../../resource/ogre_fight.png");
-        game.load.image("rose","../../resource/rose.png");
-        game.load.image("rose_fight","../../resource/rose_fight.png");
-        game.load.image("velociraptor","../../resource/velociraptor.png");
-        game.load.image("velociraptor_fight","../../resource/velociraptor_fight.png");
+        game.load.image("arcaneWisdom","./resource/arcaneWisdom.png");
+        game.load.image("dead_wing", "./resource/dead_wing.png");
+        game.load.image("dead_wing_fight", "./resource/dead_wing_fight.png");
+        game.load.image("fishman_baby", "./resource/fishman_baby.png");
+        game.load.image("fishman_baby_fight", "./resource/fishman_baby_fight.png");
+        game.load.image("freshwater_crocodile", "./resource/freshwater_crocodile.png");
+        game.load.image("freshwater_crocodile_fight", "./resource/freshwater_crocodile_fight.png");
+        game.load.image("ogre", "./resource/ogre.png");
+        game.load.image("ogre_fight", "./resource/ogre_fight.png");
+        game.load.image("rose","./resource/rose.png");
+        game.load.image("rose_fight","./resource/rose_fight.png");
+        game.load.image("velociraptor","./resource/velociraptor.png");
+        game.load.image("velociraptor_fight","./resource/velociraptor_fight.png");
 
         // 单个文件加载完的回调
         game.load.onFileComplete.add(function () {
